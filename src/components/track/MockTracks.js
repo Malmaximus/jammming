c1d39c337d2433b0be7836d2686be012259dfe7b
AugustusGB <70752219+Malmaximus@users.noreply.github.comgit config --global user.emailgit config --global user.email 70752219+Malmaximus@users.noreply.github.com>
2024-03-09 03:19:39 +0000

    finally sorted the statick tracklist component that pulls from a mock track array

---
+++ src/components/track/MockTracks.js
@@ -0,0 +1,13 @@
+const MockTracks  = [
+    { id: '1', name: 'Song 1', artist: 'Artist 1', album: 'Album 1' },
+    { id: '2', name: 'Song 2', artist: 'Artist 2', album: 'Album 2' },
+    { id: '3', name: 'Song 3', artist: 'Artist 3', album: 'Album 3' },
+    { id: '4', name: 'Song 4', artist: 'Artist 4', album: 'Album 4' },
+    { id: '5', name: 'Song 5', artist: 'Artist 5', album: 'Album 5' },
+    { id: '6', name: 'Song 6', artist: 'Artist 6', album: 'Album 6' },
+    // Add more track objects as needed
+  ];
+
+export default MockTracks ;
+
+
