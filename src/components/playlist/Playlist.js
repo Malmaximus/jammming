e459ce65fea8da7b226eb0c35a64caf00b90ef8d
AugustusGB <70752219+Malmaximus@users.noreply.github.comgit config --global user.emailgit config --global user.email 70752219+Malmaximus@users.noreply.github.com>
2024-03-14 18:54:21 +0000

    Fixed up the ability to name the playlist, now there's no seperate naming bar and instead you can click on the name itself to change it using the onBlur event listener to trigger a toggle function, and using the autoFocus method for convenience to save a click.

--- src/components/playlist/Playlist.js
+++ src/components/playlist/Playlist.js
@@ -3,24 +3,35 @@ import Track from '../track/Track';
 
 const Playlist = ({ playlistTracks, removeTrackFromPlaylist }) => {
   const [playlistName, setPlaylistName] = useState('');
+  const [isEditingName, setIsEditingName] = useState(false);
 
   const handlePlaylistNameChange = (event) => {
     setPlaylistName(event.target.value); // Update playlist name state when input value changes
   };
 
+   // Function to toggle editing mode
+   const toggleEditingName = () => {
+    setIsEditingName(!isEditingName);
+  };
+
 
   return (
     <div>
-      
-      <input
-          type="text"
-          value={playlistName} // Bind input value to playlist name state
-          onChange={handlePlaylistNameChange} // Handle input changes
-          placeholder="Enter playlist name"
-       />
-      <h2>
-        {playlistName ? playlistName : "New Playlist"} {/* if playlist has a value then use it otherwise just call it New playlist*/}
-      </h2>
+      <div>
+        {/* Conditionally render input field or h2 based on editing mode */}
+        {isEditingName ? (
+          <input
+            type="text"
+            value={playlistName} // Bind input value to playlist name state
+            onChange={handlePlaylistNameChange}  // Handle input changes
+            onBlur={toggleEditingName} // Exit editing mode on blur
+            autoFocus // Focus input field on render 
+          />
+        ) : (
+          <h2 onClick={toggleEditingName}>{playlistName ? playlistName : "New Playlist"}</h2> /* if playlist has a value then use it otherwise just call it New playlist*/
+        )}
+      </div>
+  
       <div>
         {playlistTracks.map((track) => (
           <div key={track.id} onClick={() => removeTrackFromPlaylist(track.id)}>
