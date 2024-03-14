7d1aead7f7a983c7cf1ab6e61dda8da800b28fbd
AugustusGB <70752219+Malmaximus@users.noreply.github.comgit config --global user.emailgit config --global user.email 70752219+Malmaximus@users.noreply.github.com>
2024-03-14 17:50:56 +0000

    Added ability to name the playlist within the playlist component

--- src/components/playlist/Playlist.js
+++ src/components/playlist/Playlist.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Track from '../track/Track';
 
 const Playlist = ({ playlistTracks, removeTrackFromPlaylist }) => {
+  const [playlistName, setPlaylistName] = useState('');
+
+  const handlePlaylistNameChange = (event) => {
+    setPlaylistName(event.target.value); // Update playlist name state when input value changes
+  };
+
+
   return (
     <div>
-      <h2>Playlist</h2>
+      
+      <input
+          type="text"
+          value={playlistName} // Bind input value to playlist name state
+          onChange={handlePlaylistNameChange} // Handle input changes
+          placeholder="Enter playlist name"
+       />
+      <h2>
+        {playlistName ? playlistName : "New Playlist"} {/* if playlist has a value then use it otherwise just call it New playlist*/}
+      </h2>
       <div>
         {playlistTracks.map((track) => (
           <div key={track.id} onClick={() => removeTrackFromPlaylist(track.id)}>
